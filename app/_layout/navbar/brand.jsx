b5163332fac1dec85eaa5a43267fcfcc700171b5
Luiscarlos01 <luizcarlosvitorianoneto@gmail.com>
2026-01-07 16:55:34 -0300

    fix(navbar): improve spacing and prevent text truncation
    - Adjust name spacing from left-28 to left-20 for better proximity
    - Increase hover padding from pe-8 to pe-16 to prevent 'Vitoriano' cutoff
    - Better visual balance between 'Luis' and 'Carlos Vitoriano'

--- app/_layout/navbar/brand.jsx
+++ app/_layout/navbar/brand.jsx
@@ -9,14 +9,14 @@ export function NavbarBrand() {
         <Copyright />
       </div>
 
-      <div className='relative ms-2 flex overflow-hidden whitespace-nowrap transition-all duration-500 ease-in-expo group-hover:pe-8'>
+      <div className='relative ms-2 flex overflow-hidden whitespace-nowrap transition-all duration-500 ease-in-expo group-hover:pe-16'>
         <h5 className='transition-transform duration-500 ease-in-expo group-hover:-translate-x-full'>
           Code by
         </h5>
         <h5 className='ps-1 transition-transform duration-500 ease-in-expo group-hover:-translate-x-14'>
           Luis
         </h5>
-        <h5 className='absolute left-28 ps-1 transition-transform duration-500 ease-in-expo group-hover:-translate-x-14'>
+        <h5 className='absolute left-20 ps-1 transition-transform duration-500 ease-in-expo group-hover:-translate-x-14'>
           Carlos Vitoriano
         </h5>
       </div>
